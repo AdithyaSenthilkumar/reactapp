dd12fa5425206b47242833bec49a0e3c80d45545
AdithyaSenthilkumar <146003328+AdithyaSenthilkumar@users.noreply.github.com>
2024-12-29 10:31:45 +0530

    Update Login.js

--- reactzip/frontend/invoice-extraction/src/components/Login.js
+++ reactzip/frontend/invoice-extraction/src/components/Login.js
@@ -17,7 +17,7 @@ const Login = () => {
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify({ username, password }),
       });
-      
+        
       const data = await response.json();
       
       if (response.ok) {
